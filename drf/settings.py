7b4559f4f615a7393b47af71b6086ad88d949b59
FA064207 <charugundla.ganesh@infoedge.com>
2024-09-27 15:58:42 +0530

    allowedHosts

--- drf/settings.py
+++ drf/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8y3eo5wev+vo0(=j^_kg92&e$_pscfvpeluc0r!#%8l#^fbj8g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['maniCharugundla.pythonanywhere.com','localhost']
 
 
 # Application definition
